b7b5ff136aa3d2761baf31efa3f7f82f38b44200
ko44d <koyoshid622@gmail.com>
2024-03-21 16:07:09 +0900

    Configファイルの処理を追加。またファイルの位置を移動した。

--- pkg/di.go
+++ pkg/di.go
@@ -3,11 +3,20 @@ package pkg
 import (
 	"github.com/gin-gonic/gin"
 	"ko44d/notion-api-golang-sample/cmd/notion-api-golang-sample/app"
+	"ko44d/notion-api-golang-sample/pkg/infrastructure/config"
 )
 
 type DI struct {
 }
 
-func (d DI) Init() *gin.Engine {
+func (di DI) Init() *gin.Engine {
 	return app.Init()
+}
+
+func (di DI) Config() config.Config {
+	c, err := config.GetFromEnv()
+	if err != nil {
+		panic(err)
+	}
+	return c
 }
