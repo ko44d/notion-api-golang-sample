377797135ef283379624f943bed3298470156abd
ko44d <koyoshid622@gmail.com>
2024-03-22 16:56:40 +0900

    プロパティはキー名がNotion上で使われた文字が使われる。可変のため、定義することができないのでObject扱いとする。

--- pkg/infrastructure/database/response.go
+++ pkg/infrastructure/database/response.go
@@ -11,7 +11,7 @@ type DatabaseResponse struct {
 	Url            string        `json:"url"`
 	Title          []Title       `json:"title"`
 	Description    []Description `json:"description"`
-	Properties     Properties    `json:"properties"`
+	Properties     interface{}   `json:"properties"`
 	Parent         Parent        `json:"parent"`
 	Archived       bool          `json:"archived"`
 	IsInline       bool          `json:"is_inline"`
@@ -64,125 +64,6 @@ type Description struct {
 	Annotations Annotations `json:"annotations"`
 	PlainText   string      `json:"plain_text"`
 	Href        string      `json:"href"`
-}
-
-type Properties struct {
-	PlusOne           PlusOne           `json:"+1"`
-	InStock           InStock           `json:"in_stock"`
-	Price             Price             `json:"price"`
-	Description       Desc              `json:"description"`
-	LastOrdered       LastOrdered       `json:"last_ordered"`
-	Meals             Meals             `json:"meals"`
-	NumberOfMeals     NumberOfMeals     `json:"number_of_meals"`
-	StoreAvailability StoreAvailability `json:"store_availability"`
-	Photo             Photo             `json:"photo"`
-	FoodGroup         FoodGroup         `json:"food_group"`
-	Name              Name              `json:"name"`
-}
-
-type PlusOne struct {
-	Id     string `json:"id"`
-	Name   string `json:"name"`
-	Type   string `json:"type"`
-	People string `json:"people"`
-}
-
-type InStock struct {
-	Id       string `json:"id"`
-	Name     string `json:"name"`
-	Type     string `json:"type"`
-	CheckBox string `json:"checkbox"`
-}
-
-type Price struct {
-	Id     string `json:"id"`
-	Name   string `json:"name"`
-	Type   string `json:"type"`
-	Number Number `json:"number"`
-}
-
-type Number struct {
-	Format string `json:"format"`
-}
-
-type Desc struct {
-	Id       string `json:"id"`
-	Name     string `json:"name"`
-	Type     string `json:"type"`
-	RichText string `json:"rich_text"`
-}
-
-type LastOrdered struct {
-	Id   string      `json:"id"`
-	Name string      `json:"name"`
-	Type string      `json:"type"`
-	Date interface{} `json:"date"`
-}
-
-type Meals struct {
-	Id       string   `json:"id"`
-	Name     string   `json:"name"`
-	Type     string   `json:"type"`
-	Relation Relation `json:"relation"`
-}
-
-type Relation struct {
-	DatabaseId         string `json:"database_id"`
-	SyncedPropertyName string `json:"synced_property_name"`
-}
-
-type NumberOfMeals struct {
-	Id     string `json:"id"`
-	Name   string `json:"name"`
-	Type   string `json:"type"`
-	Rollup Rollup `json:"rollup"`
-}
-type Rollup struct {
-	RollupPropertyName   string `json:"rollup_property_name"`
-	RelationPropertyName string `json:"relation_property_name"`
-	RollupPropertyId     string `json:"rollup_property_id"`
-	RelationPropertyId   string `json:"relation_property_id"`
-	Function             string `json:"function"`
-}
-
-type StoreAvailability struct {
-	Id          string      `json:"id"`
-	Name        string      `json:"name"`
-	Type        string      `json:"type"`
-	MultiSelect MultiSelect `json:"multi_select"`
-}
-
-type MultiSelect struct {
-	Id    string `json:"id"`
-	Name  string `json:"name"`
-	Color string `json:"color"`
-}
-
-type Photo struct {
-	Id    string      `json:"id"`
-	Name  string      `json:"name"`
-	Type  string      `json:"type"`
-	Files interface{} `json:"files"`
-}
-
-type FoodGroup struct {
-	Id     string `json:"id"`
-	Name   string `json:"name"`
-	Type   string `json:"type"`
-	Select Select `json:"select"`
-}
-
-type Select struct {
-	Id    string `json:"id"`
-	Name  string `json:"name"`
-	Color string `json:"color"`
-}
-
-type Name struct {
-	Id    string      `json:"id"`
-	Name  string      `json:"name"`
-	Type  string      `json:"type"`
-	Title interface{} `json:"title"`
 }
 
 type Parent struct {
